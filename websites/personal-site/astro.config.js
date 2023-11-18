c739e2636d69a8a2d3e714258f12d74ad6ceda87
Emma <MarmadileManteater@proton.me>
2023-11-17 21:11:13 -0500

    Add `@liquify/prettify` to pretty print `personal-site`

--- websites/personal-site/astro.config.js
+++ websites/personal-site/astro.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config'
 import { imagetools } from 'vite-imagetools'
 import { cleanUpImagesIn } from '@marmadilemanteater/astro-plugins/clean-images'
+import prettify from '@liquify/prettify'
+import { fileURLToPath } from 'url'
+import { join } from 'path'
+import { readFile, writeFile } from 'fs/promises'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marmadilemanteater.dev',
@@ -10,7 +14,35 @@ export default defineConfig({
     cleanUpImagesIn(
       ['images/'],
       true // check all subfolders
-    )
+    ),
+    {
+      name: '💄 Pretty print HTML w/ @liquify/prettify',
+      hooks: {
+        'astro:build:done': async function (options) {
+          const outputDir = fileURLToPath(options.dir)
+          const outputFiles = options.pages.map(({
+            pathname
+          }) => join(outputDir, pathname, 'index.html'))
+          for (const file of outputFiles) {
+            const html = (await readFile(file)).toString()
+            const prettyHtml = await prettify.format(`<!-- 🎀prettified by @liquify/prettify https://www.npmjs.com/package/@liquify/prettify -->${html
+              .replace(/<!--[^>-]*?-->/g, '\r\n')
+              .replace(/<!-- [a-zA-Z0-9-_]*? -->/g, '\r\n')
+              .replace(/> </g, '>\r\n<')
+              .replace(/></g, '>\r\n<')}`, {
+              language: 'html',
+              markup: {
+                attributeSort: true,
+                forceLeadAttribute: true,
+                ignoreScripts: true
+              }
+            })
+            await writeFile(file, prettyHtml)
+            console.log(`💄 Pretty printed '${file}'`)
+          }
+        }
+      }
+    }
   ],
   vite: {
     plugins: [imagetools()],
