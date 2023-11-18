3433cc5ca3900ceee3886e47ea7adf7f80dda12b
Emma <MarmadileManteater@proton.me>
2023-11-18 09:04:40 -0500

    Fix formatting of `pre` tags in blog

--- websites/personal-site/astro.config.js
+++ websites/personal-site/astro.config.js
@@ -5,6 +5,8 @@ import prettify from '@liquify/prettify'
 import { fileURLToPath } from 'url'
 import { join } from 'path'
 import { readFile, writeFile } from 'fs/promises'
+import jsdom from 'jsdom'
+const { JSDOM } = jsdom
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marmadilemanteater.dev',
@@ -24,10 +26,17 @@ export default defineConfig({
             pathname
           }) => join(outputDir, pathname, 'index.html'))
           for (const file of outputFiles) {
-            const html = (await readFile(file)).toString()
-            const prettyHtml = await prettify.format(`<!-- 🎀prettified by @liquify/prettify https://www.npmjs.com/package/@liquify/prettify -->${html
-              .replace(/<!--[^>-]*?-->/g, '\r\n')
-              .replace(/<!-- [a-zA-Z0-9-_]*? -->/g, '\r\n')
+            let html = (await readFile(file)).toString()
+            const doc = new JSDOM(html)
+            html = `\r\n<!DOCTYPE HTML>\r\n${doc.window.document.body.parentElement.outerHTML}`
+            const pres = Array.from(doc.window.document.querySelectorAll('pre')).map((pre) => pre.outerHTML)
+            // replace all `pre` elements with placeholders
+            for (let i = 0; i < pres.length; i++) {
+              console.log(pres[i])
+              html = html.replace(pres[i], `<pre-${i} />`)
+            }
+
+            html = await prettify.format(`<!-- 🎀prettified by @liquify/prettify https://www.npmjs.com/package/@liquify/prettify -->${html
               .replace(/> </g, '>\r\n<')
               .replace(/></g, '>\r\n<')}`, {
               language: 'html',
@@ -37,7 +46,11 @@ export default defineConfig({
                 ignoreScripts: true
               }
             })
-            await writeFile(file, prettyHtml)
+            // replace `pre` placeholders with their original contents
+            for (let i = 0; i < pres.length; i++) {
+              html = html.replace(`<pre-${i}/>`, pres[i])
+            }
+            await writeFile(file, html)
             console.log(`💄 Pretty printed '${file}'`)
           }
         }
